fc63d317fb0e91ebed82cf6e6262c6e7a1b37b19
Ed1l3udo <edileudommfilho@gmail.com>
2025-04-15 08:54:15 -0300

    "Alterações atrasadas da aula 2"

--- src/running_example/Banco.java
+++ src/running_example/Banco.java
@@ -12,4 +12,64 @@ public class Banco {
 		contas[indice] = conta;
 		indice++;
 	}
+	
+	private Conta procurar(String numero) {
+		int i = 0;
+		boolean encontrou = false;
+		while(!encontrou && i < indice) {
+			if(contas[i].getNumero() == numero) {
+				encontrou = true;
+			}else {
+				i++;
+			}
+		}
+		if(encontrou) {
+			return contas[i];
+		}else {
+			return null;
+		}
+	}
+	
+	public void creditar(String numero, double valor) {
+		Conta conta = procurar(numero);
+		if(conta == null) {
+			System.out.println("Conta inexistente");
+		}else {
+			conta.creditar(valor);
+		}
+	}
+	
+	public void debitar(String numero, double valor) {
+		Conta conta = procurar(numero);
+		if(conta == null) {
+			System.out.println("Conta inexistente");
+		}else {
+			conta.debitar(valor);
+		}
+	}
+	
+	public double saldo(String numero) {
+		Conta conta = procurar(numero);
+		if(conta == null) {
+			System.out.println("Conta inexistente");
+			return 0;
+		}else {
+			return conta.getSaldo();
+		}
+	}
+	
+	public void transferir(String origem, String destino, double valor) {
+		Conta contaOrigem = procurar(origem);
+		Conta contaDestino = procurar(destino);
+		if(contaOrigem == null) {
+			System.out.println("Conta remetente inexistente");
+		}
+		if(contaDestino == null) {
+			System.out.println("Conta destinatária inexistente");
+		}
+		if(contaDestino != null && contaOrigem != null) {
+			contaOrigem.debitar(valor);
+			contaDestino.creditar(valor);
+		}
+	}
 }
