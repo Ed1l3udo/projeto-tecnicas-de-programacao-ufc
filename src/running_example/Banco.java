3c30fa2fba518bf389beda0b1c4aa00a370a510a
Ed1l3udo <edileudommfilho@gmail.com>
2025-04-14 22:34:28 -0300

    Primeiro commit

---
+++ src/running_example/Banco.java
@@ -0,0 +1,5 @@
+package running_example;
+
+public class Banco {
+
+}
---
+++ src/running_example/Main.java
@@ -0,0 +1,11 @@
+package running_example;
+
+public class Main {
+	public static void main(String args[]) {
+		Conta conta;
+		conta = new Conta("21.342-7");
+		conta.creditar(500.87);
+		conta.debitar(45);
+		System.out.println("O saldo é: " + conta.getSaldo());
+	}
+}
