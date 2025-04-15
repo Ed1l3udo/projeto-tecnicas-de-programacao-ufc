55bb37112e2ca426367bed0760064a87d0500120
Ed1l3udo <edileudommfilho@gmail.com>
2025-04-14 22:42:54 -0300

    Arrumando classe Banco

--- src/running_example/Banco.java
+++ src/running_example/Banco.java
@@ -1,5 +1,15 @@
 package running_example;
 
 public class Banco {
-
+	private Conta[] contas;
+	private int indice = 0;
+	
+	public Banco() {
+		contas = new Conta[100];
+	}
+	
+	public void cadastrar(Conta conta) {
+		contas[indice] = conta;
+		indice++;
+	}
 }
